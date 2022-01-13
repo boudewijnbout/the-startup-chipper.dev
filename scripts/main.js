b4c9806adb7340164f0ee07e59313901682e21ad
boudewijnbout <boudewijn_bout@outlook.com>
2022-01-13 13:32:28 +0100

    Banner v1

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const navbar = document.querySelector('header');
-const svg  = document.querySelector('header svg');
+const svg = document.querySelector('header svg');
 
-window.onscroll = function() {
+window.onscroll = function () {
     if (window.pageYOffset > 0) {
         navbar.classList.add('scrolled');
         svg.classList.add('toggle');
@@ -10,3 +10,17 @@ window.onscroll = function() {
         svg.classList.remove('toggle');
     }
 }
+
+const darkmodeToggle = document.querySelector('.darkmode-switch');
+
+darkmodeToggle.addEventListener('click', function () {
+
+    // Header
+    document.querySelector('header .btn-secondary').classList.toggle('dark');
+
+    // Banner
+    document.querySelector('.banner').classList.toggle('dark');
+    document.querySelector('.banner h1').classList.toggle('dark');
+    document.querySelector('.test').classList.toggle('dark');
+    document.querySelector('.banner a').classList.toggle('dark');
+});
